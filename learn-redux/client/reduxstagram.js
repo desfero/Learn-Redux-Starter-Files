62dbaa9751c10e1f285993d9ed865c3212364ede
Pavlo Liulia <pliulia@pgs-soft.com>
2016-06-04 08:15:26 +0200

    up to 20 lessons

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -9,10 +9,15 @@ import App from './components/App'
 import Single from './components/Single'
 import PhotoGrid from './components/PhotoGrid'
 
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
+
 // Import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store'
+
+//Raven.config(sentry_url).install();
 
 const router = (
   <Provider store={store}>
